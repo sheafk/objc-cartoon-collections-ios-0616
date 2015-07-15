5e344338ad1ce2d4e6895ffbe4f4018a435a9130
markedwardmurray <markedwardmurray@gmail.com>
2015-07-15 12:05:09 -0400

    remove solution

--- cartoonCollections/FISAppDelegate.h
+++ cartoonCollections/FISAppDelegate.h
@@ -12,15 +12,10 @@
 
 @property (strong, nonatomic) UIWindow *window;
 
--(NSString *)stringByRollCallingDwarfsInArray:(NSArray *)dwarfs;
-
--(NSArray *)arrayOfPlaneteerShoutsFromArray:(NSArray *)powers;
-
--(NSString *)summonCaptainPlanetWithPowers:(NSArray *)powers;
-
--(NSString *)findFirstOfPremiumCheeses:(NSArray *)premiumCheeses
-                      inCheesesInStock:(NSArray *)cheesesInStock;
-
--(NSArray *)arrayByConvertingMoneyBagsIntoPaperBills:(NSArray *)moneyBags;
+/**
+ 
+ * Declare your methods here.
+ 
+ */
 
 @end
--- cartoonCollections/FISAppDelegate.m
+++ cartoonCollections/FISAppDelegate.m
@@ -16,74 +16,10 @@
     return YES;
 }
 
-- (NSString *)stringByRollCallingDwarfsInArray:(NSArray *)dwarfs {
-    
-    NSMutableString *rollCall = [[NSMutableString alloc] init];
-    
-    NSUInteger lastIndex = dwarfs.count - 1;
-    
-    for (NSUInteger i = 0; i < dwarfs.count; i++) {
-        
-        [rollCall appendFormat:@"%lu. %@", i + 1, dwarfs[i]];
-        
-        if (i < lastIndex) {
-            [rollCall appendString:@" | "];
-        }
-    }
-    
-    return [NSString stringWithString:rollCall];
-}
-
-- (NSArray *)arrayOfPlaneteerShoutsFromArray:(NSArray *)powers{
-    
-    NSMutableArray *powersAsShouts = [[NSMutableArray alloc] init];
-    
-    for (NSString *power in powers) {
-        NSString *uppercasePower = [power uppercaseString];
-        NSString *powerAsShout = [uppercasePower stringByAppendingString:@"!"];
-        
-        [powersAsShouts addObject:powerAsShout];
-    }
-    return [NSArray arrayWithArray:powersAsShouts];
-}
-
-- (NSString *)summonCaptainPlanetWithPowers:(NSArray *)powers {
-    
-    NSMutableString *summons = [[NSMutableString alloc] initWithString:@"Let our powers combine:\n"];
-    
-    NSArray *powersAsShouts = [self arrayOfPlaneteerShoutsFromArray:powers];
-    
-    for (NSString *shout in powersAsShouts) {
-        [summons appendFormat:@"%@\n", shout];
-    }
-    
-    [summons appendString:@"Go Planet!"];
-    
-    return [NSString stringWithString:summons];
-}
-
-- (NSString *)findFirstOfPremiumCheeses:(NSArray *)premiumCheeses
-                       inCheesesInStock:(NSArray *)cheesesInStock {
-    
-    for (NSString *premiumCheese in premiumCheeses) {
-        if ([cheesesInStock containsObject:premiumCheese]) {
-            return premiumCheese;
-        }
-    }
-    return @"No premium cheeses in stock.";
-}
-
-- (NSArray *)arrayByConvertingMoneyBagsIntoPaperBills:(NSArray *)moneyBags {
-    
-    NSMutableArray *paperBills = [[NSMutableArray alloc] init];
-    
-    for (NSString *moneyBag in moneyBags) {
-        NSString *paperBill = [NSString stringWithFormat:@"$%lu", moneyBag.length];
-        [paperBills addObject:paperBill];
-    }
-    
-    return [NSArray arrayWithArray:paperBills];
-}
-
+/**
+ 
+ *  Define your method implementations here.
+ 
+ */
 
 @end
