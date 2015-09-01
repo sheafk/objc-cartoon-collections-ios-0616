2d56675d126157eb66b2ae86cfa065af6e2490c7
markedwardmurray <markedwardmurray@gmail.com>
2015-09-01 00:21:41 -0400

    solution: simplify implementations

--- cartoonCollections/FISAppDelegate.m
+++ cartoonCollections/FISAppDelegate.m
@@ -20,9 +20,9 @@
     
     NSMutableString *rollCall = [[NSMutableString alloc] init];
     
-    NSUInteger lastIndex = dwarfs.count - 1;
+    NSUInteger lastIndex = [dwarfs count] - 1;
     
-    for (NSUInteger i = 0; i < dwarfs.count; i++) {
+    for (NSUInteger i = 0; i < [dwarfs count]; i++) {
         
         [rollCall appendFormat:@"%lu. %@", i + 1, dwarfs[i]];
         
@@ -31,45 +31,52 @@
         }
     }
     
-    return [NSString stringWithString:rollCall];
+    return rollCall;
 }
 
 - (NSArray *)arrayOfPlaneteerShoutsFromArray:(NSArray *)powers{
     
     NSMutableArray *powersAsShouts = [[NSMutableArray alloc] init];
     
-    for (NSString *power in powers) {
+    for (NSUInteger i = 0; i < [powers count]; i++) {
+        NSString *power = powers[i];
         NSString *uppercasePower = [power uppercaseString];
         NSString *powerAsShout = [uppercasePower stringByAppendingString:@"!"];
         
         [powersAsShouts addObject:powerAsShout];
     }
-    return [NSArray arrayWithArray:powersAsShouts];
+    return powersAsShouts;
 }
 
 - (NSString *)summonCaptainPlanetWithPowers:(NSArray *)powers {
     
-    NSMutableString *summons = [[NSMutableString alloc] initWithString:@"Let our powers combine:\n"];
+    NSMutableString *summons = [[NSMutableString alloc] init];
+    [summons appendString:@"Let our powers combine:"];
     
     NSArray *powersAsShouts = [self arrayOfPlaneteerShoutsFromArray:powers];
     
-    for (NSString *shout in powersAsShouts) {
-        [summons appendFormat:@"%@\n", shout];
+    for (NSUInteger i = 0; i < [powersAsShouts count]; i++) {
+        NSString *shout = powersAsShouts[i];
+        
+        [summons appendFormat:@"\n%@", shout];
     }
     
-    [summons appendString:@"Go Planet!"];
+    [summons appendString:@"\nGo Planet!"];
     
-    return [NSString stringWithString:summons];
+    return summons;
 }
 
 - (NSString *)firstPremiumCheeseInStock:(NSArray *)cheesesInStock
                      premiumCheeseNames:(NSArray *)premiumCheeseNames {
     
-    for (NSString *premiumCheese in premiumCheeseNames) {
+    for (NSUInteger i = 0; i < [premiumCheeseNames count]; i++) {
+        NSString *premiumCheese = premiumCheeseNames[i];
+        
         if ([cheesesInStock containsObject:premiumCheese]) {
             return premiumCheese;
         }
     }
+    
     return @"No premium cheeses in stock.";
 }
 
@@ -77,12 +84,15 @@
     
     NSMutableArray *paperBills = [[NSMutableArray alloc] init];
     
-    for (NSString *moneyBag in moneyBags) {
-        NSString *paperBill = [NSString stringWithFormat:@"$%lu", moneyBag.length];
+    for (NSUInteger i = 0; i < [moneyBags count]; i++) {
+        NSString *moneyBag = moneyBags[i];
+        NSUInteger moneyBagLength = [moneyBag length];
+        NSString *paperBill = [NSString stringWithFormat:@"$%lu", moneyBagLength];
+        
         [paperBills addObject:paperBill];
     }
     
-    return [NSArray arrayWithArray:paperBills];
+    return paperBills;
 }
 
 
